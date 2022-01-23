429634386252f6652d15af76b76d8672a64923b9
Marya Samsonenko <samashushka7@gmail.com>
2022-01-23 03:20:55 +0200

    add loader

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from "react";
 import { ToastContainer, Zoom, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Grid } from "react-loader-spinner";
 import { apiServices } from "../../services/api";
 import { Searchbar } from "../Searchbar/Searchbar";
 import { ImageGallery } from "../ImageGallery/ImageGallery";
 import { Button } from "../Button/Button";
 import { Modal } from "../Modal/Modal";
-import { Container } from "./App.styled";
+import { Container, Wrapper } from "./App.styled";
 
 export class App extends Component {
   state = {
     searchQuery: "",
     page: 1,
     imageCards: [],
-    isLoading: false,
+    loading: false,
     showModal: false,
     largeImage: "",
     error: null,
     totalPages: null,
-  };
-  handleFofmSubmit = (searchQuery) => {
-    return this.setState({ searchQuery, page: 1, imageCards: [] });
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -31,15 +30,20 @@ export class App extends Component {
       this.fetchImages();
     }
   }
+
+  handleFofmSubmit = (searchQuery) => {
+    return this.setState({ searchQuery, page: 1, imageCards: [] });
+  };
+
   fetchImages = async () => {
     const { searchQuery, page } = this.state;
 
     try {
-      this.setState({ isLoading: true });
+      this.toggleLoader(true);
       const data = await apiServices({ searchQuery, page });
       if (data.hits.length === 0) {
         toast.warn("Nothing found with your search query");
-        this.setState({ isLoading: false, totalPages: null });
+        this.setState({ loading: false, totalPages: null });
 
         return;
       }
@@ -49,34 +53,41 @@ export class App extends Component {
         totalPages: Math.ceil(data.totalHits / 12),
         page: prevState.page + 1,
       }));
-      console.log(this.state.largeImage);
     } catch (error) {
       console.log(error);
       this.setState({ error: error.message });
     } finally {
-      this.setState({ loading: false });
+      this.toggleLoader(false);
     }
   };
 
   setModalImage = (imageLink) => {
     return this.setState((prevState) => ({ largeImage: imageLink }));
   };
+
   openLargeImage = (imageLink) => {
     this.setModalImage(imageLink);
     this.toggleModal();
   };
 
   handleOnClick = () => {
+    this.toggleLoader(true);
     this.fetchImages();
   };
+
   toggleModal = () => {
     this.setState(({ showModal }) => ({ showModal: !showModal }));
   };
+
+  toggleLoader = (boolean) => {
+    this.setState(({ loading }) => ({ loading: boolean }));
+  };
+
   render() {
     const {
       imageCards,
       totalPages,
-      isLoading,
+      loading,
       error,
       page,
       showModal,
@@ -93,7 +104,7 @@ export class App extends Component {
               onClick={this.toggleModal}
               modalOpen={this.openLargeImage}
             />
-            {totalPages > 1 && totalPages !== page - 1 && (
+            {totalPages > 1 && totalPages !== page - 1 && !loading && (
               <Button onClick={this.handleOnClick} />
             )}
           </>
@@ -103,6 +114,12 @@ export class App extends Component {
             <img src={largeImage} alt="largeImage" />
           </Modal>
         )}
+        {loading && (
+          <Wrapper>
+            <Grid heigth="80" width="80" color="#303f9f" ariaLabel="loading" />
+          </Wrapper>
+        )}
+        {error && <h3 className="Message">Oops, please try again</h3>}
         <ToastContainer limit={3} autoClose={2000} transition={Zoom} />
       </Container>
     );
--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -6,3 +6,6 @@ export const Container = styled.div`
   grid-gap: 16px;
   padding-bottom: 24px;
 `;
+export const Wrapper = styled.div`
+  margin: 0 auto;
+`;
